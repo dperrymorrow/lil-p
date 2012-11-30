ee6b5dcf95cd94dcc886f5d307faee25ce81a802
David Morrow <dmorrow@newrelic.com>
2012-11-29 22:19:43 -0800

    optimized some of the string prototype utils updated several tests changed the names of trim, rtrim and ltrim

--- tests/string_tests.js
+++ tests/string_tests.js
@@ -1,7 +1,7 @@
 /*global setTimeout:true, equal:true, stop:true, start:true, test:true*/
 /*jslint browser: true, white: false, vars: true, devel: true, bitwise: true, debug: true, nomen: true, sloppy: true, indent: 2*/
 
-// testing string extensions
+// testing truncation
 test("truncates properly", function () {
   equal('Hey there im a really long string'.truncate(5), 'Hey t...');
 });
@@ -18,22 +18,24 @@ test("expect foobar to conain foo", function () {
   equal("foobar".contains('foo'), true);
 });
 
-test("pluralize with a value", function () {
-  equal('dog'.pluralize(2), 'dogs');
-});
-
-test("pluralize with noting passed", function () {
-  equal('dog'.pluralize(), 'dog');
-});
-
-test("singularize", function () {
-  equal('dogs'.singularize(), 'dog');
-});
-
+// testing contains
 test("expect foobar not to contain donkey", function () {
   equal("foobar".contains('donkey'), false);
 });
 
+test("pluralize with a value", function () {
+  equal('dog'.pluralize(2), 'dogs');
+});
+
+// pluralize
+test("pluralize with noting passed", function () {
+  equal('dog'.pluralize(), 'dog');
+});
+// singularize
+test("singularize", function () {
+  equal('dogs'.singularize(), 'dog');
+});
+// wordwrap testing
 test("splits on characters correctly", function () {
   equal("hello there-somethingerather".wordWrap(), "hello <wbr>there-<wbr>somethingerather");
 });
